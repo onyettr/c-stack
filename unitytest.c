423398f6778191f1d770d8267e380bcb3e5e6054
Richard Onyett <ronyett@megachips.com>
2019-03-15 13:48:53 -0700

    missed a file

---
+++ unitytest.c
@@ -0,0 +1,53 @@
+/**
+ *****************************************************************************
+ * MODULE: C Programming Examples
+ *
+ * @author onyettr
+ * PURPOSE: unity test harness for Simple Stack 
+ ***************************************************************************** 
+ */
+
+/*
+******************************************************************************
+Includes
+******************************************************************************
+*/
+#include <stdio.h>
+#include <stdlib.h>
+#include "stack.h"
+#include "unity.h"
+
+void stack_create_invalid_size(void) {
+  Stack_t *sp;
+
+  sp = StackCreate(-1);
+  TEST_ASSERT_MESSAGE(sp == NULL, "Stack rturned NULL");
+}
+
+void stack_create_valid_size(void) {
+  Stack_t *sp;
+
+  sp = StackCreate(10);
+  TEST_ASSERT_MESSAGE(sp != NULL, "Stack created ok");
+}
+
+void runTest(UnityTestFunction test) {
+  if(TEST_PROTECT()) {
+    test();
+  }
+}
+void setUp(void) {
+}
+
+void tearDown(void) {
+}
+
+int main ( void ) {
+  UnityBegin("stack.c");
+
+  RUN_TEST(stack_create_invalid_size, __LINE__);
+  RUN_TEST(stack_create_valid_size,   __LINE__);
+  UnityEnd();
+
+  return 0;
+}
